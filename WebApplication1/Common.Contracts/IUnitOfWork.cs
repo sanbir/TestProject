7bec25c3da137fef5f8307e26603c753d63a221b
Alexander Biryukov <biryukovao@edu.stu.ru>
2015-05-03 17:52:01 +0600

    just stuff

---
+++ WebApplication1/Common.Contracts/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Contracts
+{
+    public interface IUnitOfWork
+    {
+        void Commit();
+    }
+
+    public interface IUnitOfWork<T> : IUnitOfWork
+        where T : class, new()
+    {
+        IEnumerable<IDataRepository<T>> Get();
+    }
+}
