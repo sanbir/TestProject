e9791b38bd53047c677b0a4b7743377fe4a2bfcc
Alexander Biryukov <biryukovao@edu.stu.ru>
2015-05-10 19:46:42 +0600

    filter

---
+++ WebApplication1/PresentationLayer/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Data.Models;
+using PagedList;
+
+namespace ContosoUniversity.ViewModels
+{
+    public class EmployeeViewModel
+    {
+        //public IPagedList<Employee> Employees { get; set; }
+        //public 
+    }
+}
