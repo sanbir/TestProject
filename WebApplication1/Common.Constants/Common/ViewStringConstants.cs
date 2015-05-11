9b9953e4f231f70937b257c087e12c303c6824bb
Alexander Biryukov <biryukovao@edu.stu.ru>
2015-05-11 18:08:53 +0600

    moar const

---
+++ WebApplication1/Common.Constants/Common/ViewStringConstants.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Constants.Common
+{
+    public static class ViewStringConstants
+    {
+        public const string Create = "Создать";
+        public const string Delete = "Удалить";
+        public const string AreYouSure = "Вы действительно хотите удалить?";
+        public const string Details = "Просмотр";
+        public const string Edit = "Редактировать";
+        public const string Save = "Сохранить";
+        public const string EmployeeTitle = "Работник";
+        public const string EmployeesTitle = "Работники";
+        public const string Back = "Назад";
+        public const string Search = "Поиск";
+    }
+}
--- WebApplication1/Common.Constants/Employee/EmployeeProperties.cs
+++ WebApplication1/Common.Constants/Employee/EmployeeProperties.cs
@@ -7,11 +7,13 @@
         public const string MiddleName = "MiddleName";
         public const string Email = "Email";
         public const string ContractorCompanyName = "ContractorCompanyName";
+
         public const string FirstNameDisplay = "Имя";
         public const string LastNameDisplay = "Фамилия";
         public const string MiddleNameDisplay = "Отчество";
         public const string EmailDisplay = "E-mail";
         public const string ContractorCompanyNameDisplay = "Название компании-исполнителя";
+
         public const string BindEmployeeProperties = "LastName, FirstName, MiddleName, Email, ContractorCompanyName";
 
     }
