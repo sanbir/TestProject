fd58a57399291ce0691879e1c9f6dda15ef9267c
Alexander Biryukov <biryukovao@edu.stu.ru>
2015-05-19 00:05:44 +0600

    Revert "Revert "add angular table""
    This reverts commit 73d7e983036ac6e2ea174496fb992fe17b6b5b5c.

--- WebApplication1/WebApplication/js/EmployeeIndex/EmployeeIndexDirectives.js
+++ WebApplication1/WebApplication/js/EmployeeIndex/EmployeeIndexDirectives.js
@@ -1,12 +1,43 @@
 ﻿(function () {
-    var employeeIndexDirectives = function () {
+    var customSort = function () {
+        return {
+            restrict: 'A',
+            transclude: true,
+            scope: {
+                order: '=',
+                sort: '='
+            },
+            template:
+              ' <a ng-click="sort_by(order)" style="color: #555555;">' +
+              '    <span ng-transclude></span>' +
+              '    <i ng-class="selectedCls(order)"></i>' +
+              '</a>',
+            link: function (scope) {
 
-        var factory = {};
+                // change sorting order
+                scope.sort_by = function (newSortingOrder) {
+                    var sort = scope.sort;
 
-        factory.Id = 42;
+                    if (sort.sortingOrder == newSortingOrder) {
+                        sort.reverse = !sort.reverse;
+                    }
 
-        return factory;
+                    sort.sortingOrder = newSortingOrder;
+                };
+
+
+                scope.selectedCls = function (column) {
+                    if (column == scope.sort.sortingOrder) {
+                        return ('icon-chevron-' + ((scope.sort.reverse) ? 'down' : 'up'));
+                    }
+                    else {
+                        return 'icon-sort';
+                    }
+                };
+            }// end link
+        }
     };
 
-    angular.module('EmployeeIndexApp').directive('EmployeeIndexDirectives', employeeIndexDirectives);
+    angular.module('EmployeeIndexApp').directive('customSort', customSort);
+
 }());
