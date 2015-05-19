26f7e0a895b03bf5d3fef45bf988fa299516075e
Alexander Biryukov <biryukovao@edu.stu.ru>
2015-05-19 21:42:57 +0600

    checkbox directive works

--- WebApplication1/WebApplication/js/ProjectAssignEmployeesApp/Directives/SetManager.js
+++ WebApplication1/WebApplication/js/ProjectAssignEmployeesApp/Directives/SetManager.js
@@ -1,1 +1,24 @@
-﻿
+﻿(function () {
+    var setManager = function () {
+        return {
+            require: 'ngModel',
+            link: function (scope, element, attrs, ngModelController) {
+                ngModelController.$parsers.push(function (isManager) {
+
+                    //TODO:
+                    var selectedEmployee = scope.item;
+                    var managerId = scope.$parent.project.managerId;
+
+                    if (isManager) {
+                        managerId = selectedEmployee.id;
+                    }
+
+                    return isManager;
+                });
+            }
+        };
+    };
+
+    angular.module('projectAssignEmployeesApp').directive('setManager', setManager);
+
+}());
