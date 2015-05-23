d167608d8d2db538643e0ebc2871902c4b936126
Alexander Biryukov <biryukovao@edu.stu.ru>
2015-05-24 02:23:59 +0600

    start js

--- WebApplication1/WebApplication/js/ProjectAssignEmployeesApp/ProjectAssignEmployeesController.js
+++ WebApplication1/WebApplication/js/ProjectAssignEmployeesApp/ProjectAssignEmployeesController.js
@@ -33,7 +33,7 @@
         $scope.sendData = function () {
             $scope.response = '';
 
-            var httpRequest = httpRequestHandler('post', '/Project/Persist', JSON.stringify($scope.project));
+            var httpRequest = httpRequestHandler('POST', '/Project/Persist', JSON.stringify($scope.project));
 
             httpRequest.then(function (data) {
                 $scope.response = data;
@@ -79,6 +79,33 @@
             return result.promise;
         }
 
+        /////////////////////////////////////////
+
+        var sortDirections = {
+            ASC: "Ascending",
+            DESC: "Descending"
+        };
+
+        $scope.sortDirection = sortDirections.ASC;
+        $scope.sortPropertyName = "";
+        $scope.searchString = "";
+        $scope.page = 1;
+
+        $scope.getEmployees = function () {
+            $scope.response = '';
+
+            var httpRequest = httpRequestHandler('GET', '/Project/GetEmployees', JSON.stringify($scope.project));
+
+            httpRequest.then(function (data) {
+                $scope.employeesPage = data;
+
+            }, function (error) {
+                $scope.response = error;
+            });
+        };
+        $scope.getEmployees();
+
+
 
         // init
         $scope.sort = {
@@ -94,79 +121,25 @@
         $scope.pagedItems = [];
         $scope.currentPage = 1;
         $scope.items = $scope.employeesPage;
-
-        var searchMatch = function (haystack, needle) {
-            if (!needle) {
-                return true;
-            }
-            return haystack.toLowerCase().indexOf(needle.toLowerCase()) !== -1;
-        };
+        $scope.query = "";
 
         // init the filtered items
         $scope.search = function () {
-            $scope.filteredItems = $filter('filter')($scope.items, function (item) {
-                for (var attr in item) {
-                    if (searchMatch(item[attr], $scope.query))
-                        return true;
-                }
-                return false;
-            });
-            // take care of the sorting order
-            if ($scope.sort.sortingOrder !== '') {
-                $scope.filteredItems = $filter('orderBy')($scope.filteredItems, $scope.sort.sortingOrder, $scope.sort.reverse);
-            }
-            $scope.currentPage = 0;
-            // now group by pages
-            $scope.groupToPages();
-        };
 
-
-        // calculate page in place
-        $scope.groupToPages = function () {
-            $scope.pagedItems = [];
-
-            for (var i = 0; i < $scope.filteredItems.length; i++) {
-                if (i % $scope.itemsPerPage === 0) {
-                    $scope.pagedItems[Math.floor(i / $scope.itemsPerPage)] = [$scope.filteredItems[i]];
-                } else {
-                    $scope.pagedItems[Math.floor(i / $scope.itemsPerPage)].push($scope.filteredItems[i]);
-                }
-            }
-        };
-
-        $scope.range = function (size, start, end) {
-            var ret = [];
-            console.log(size, start, end);
-
-            if (size < end) {
-                end = size;
-                start = size - $scope.gap;
-            }
-            for (var i = start; i < end; i++) {
-                ret.push(i);
-            }
-            console.log(ret);
-            return ret;
         };
 
         $scope.prevPage = function () {
-            if ($scope.currentPage > 0) {
+            if ($scope.currentPage > 1) {
                 $scope.currentPage--;
             }
         };
 
         $scope.nextPage = function () {
-            if ($scope.currentPage < $scope.pagedItems.length - 1) {
+            if ($scope.currentPage < $scope.pagedItems.length - 2) {
                 $scope.currentPage++;
             }
         };
 
-        $scope.setPage = function () {
-            $scope.currentPage = this.n;
-        };
-
-        // functions have been describe process the data for display
-        $scope.search();
     };
 
     projectAssignEmployeesController.$inject = ['$scope', '$filter', '$http', '$q', 'projectFactory', 'employeesPageFactory'];
