12756678f154d18821c27cd6a8c397d8fa535ada
Alexander Biryukov <biryukovao@edu.stu.tu>
2015-05-27 10:00:39 +0600

    start with removing page classes

--- WebApplication1/Shared.Constants/Project/ProjectValidationMessages.cs
+++ WebApplication1/Shared.Constants/Project/ProjectValidationMessages.cs
@@ -6,7 +6,7 @@
         public const string EnterCustomerCompanyName = "Введите название компании-заказчика";
         public const string EnterManagerId = "Назначьте руководителя проекта";
         public const string EnterStartDate = "Введите дату начала проекта";
-        public const string DateFormatString = "{0:dd.MM.yyyy}";
+        public const string DateFormatString = "{0:MM-dd-yyyy}";
         public const string EnterEndDate = "Введите дату окончания проекта";
         public const string EnterPriority = "Введите приоритет проекта";
         public const string PriorityRange = "Приоритетом проекта должно быть целочисленное значение >= 0";
