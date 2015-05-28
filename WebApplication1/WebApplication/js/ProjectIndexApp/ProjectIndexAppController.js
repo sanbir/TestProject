d545f922fca94aaff528544fe8ddf0f1e651a3ce
Alexander Biryukov <biryukovao@edu.stu.tu>
2015-05-28 13:02:40 +0600

    playing with glyph

--- WebApplication1/WebApplication/js/ProjectIndexApp/ProjectIndexAppController.js
+++ WebApplication1/WebApplication/js/ProjectIndexApp/ProjectIndexAppController.js
@@ -2,7 +2,27 @@
 
     var projectIndexController = function ($scope) {
 
-        $scope.temp = "temp!!!";
+        var commonFilter = function () {
+            this.isShown = get("SearchString");
+            this.show = function() {
+                this.isShown = !this.isShown;
+            }
+        };
+
+        $scope.projectNameFilter = new commonFilter();
+        $scope.customerCompanyNameFilter = new commonFilter();
+        $scope.managerFilter = new commonFilter();
+        $scope.startDateFilter = new commonFilter();
+        $scope.endDateFilter = new commonFilter();
+        $scope.priorityFilter = new commonFilter();
+        $scope.commentFilter = new commonFilter();
+
+        function get(name) {
+            if (name = (new RegExp('[?&]' + encodeURIComponent(name) + '=([^&]*)')).exec(location.search)) {
+                if (decodeURIComponent(name[1]) !== "") return true;
+            }
+            return false;
+        }
 
     };
 
