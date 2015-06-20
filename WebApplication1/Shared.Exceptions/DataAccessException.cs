2a52c88ce7230ab2ea4674f111d8e3f2f769f489
Alexander Biryukov <biryukovao@edu.stu.ru>
2015-06-20 21:28:00 +0600

    add exception handling (message)

---
+++ WebApplication1/Shared.Exceptions/DataAccessException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Exceptions
+{
+    public class DataAccessException : ApplicationException
+    {
+        public DataAccessException(string message)
+            : this(message, null)
+        {
+        }
+
+        public DataAccessException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        public override string Message
+        {
+            get { return "DataAccessException"; }
+        }
+    }
+}
