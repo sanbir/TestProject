3da3cea5d8887bce37826a27482293a2f6da0187
Alexander Biryukov <biryukovao@edu.stu.ru>
2015-06-26 00:05:34 +0600

    temp

--- WebApplication1/Shared.Constants/Common/ErrorMessages.cs
+++ WebApplication1/Shared.Constants/Common/ErrorMessages.cs
@@ -2,8 +2,8 @@
 {
     public static class ErrorMessages
     {
-        public const string CouldNotSave = "Не удалось сохранить. Пожалуйста, попробуйте позже";
-        public const string CouldNotDelete = "Не удалось удалить. Пожалуйста, попробуйте позже";
+        public const string CouldNotSave = "Не удалось сохранить. Пожалуйста, попробуйте позже.";
+        public const string CouldNotDelete = "Не удалось удалить. Пожалуйста, попробуйте позже.";
 
     }
 }
